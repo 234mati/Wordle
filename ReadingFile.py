53c3547843934b058e3950bb35e9b74e33b9b838
234mati <mateusz.pietrych@gmail.com>
2022-06-11 16:32:02 +0200

    Functioning game in console

--- ReadingFile.py
+++ ReadingFile.py
@@ -4,35 +4,42 @@ import numpy as np
 import random
 import os
 import Node
+from configparser import ConfigParser
+import datetime
 
 
 
 class ReadingFile():
-    def __init__(self, minWordLength, maxWordLength, treeDeep):
-        self.minWordLength = minWordLength
-        self.maxWordLength = maxWordLength
-        self.treeDeep = treeDeep
-        
-        scriptDir = os.path.dirname(__file__)
-        textOpenForCheckingPath  = "Data/slowa.txt"
-        textOpenGuessWordsPath  = "Data/DoWyboru.txt"
+    def __init__(self):
+        self.scriptDir = os.path.dirname(__file__)
+        self.configFile = self.read_config()
 
-        f = open(os.path.join(scriptDir, textOpenForCheckingPath), encoding="utf8")
-        textOpenForChecking = f.read()
-        f.close()
-        #with open(..) as f:
-        f2 = open(os.path.join(scriptDir, textOpenGuessWordsPath), encoding="utf8")
-        textOpenGuessWords = f2.read()
-        f2.close()
+        scriptDir = os.path.dirname(__file__)
+        self.scoresPath = self.configFile['paths']['scoresPath']
+        self.savedGamesPath = self.configFile['paths']['savedGamesPath']
+        textOpenForCheckingPath  = self.configFile['paths']['checkWordsPath']
+        textOpenGuessWordsPath  = self.configFile['paths']['guessWordsPath']
+
+        self.minWordLength = int(self.configFile['base']['minWordLength'])
+        self.maxWordLength = int(self.configFile['base']['maxWordLength'])
+        self.treeDeep = int(self.configFile['base']['treeDeep'])
+ 
+
+        with open(os.path.join(scriptDir, textOpenForCheckingPath), encoding="utf8") as f:
+            textOpenForChecking = f.read()
+        
+        with open(os.path.join(scriptDir, textOpenGuessWordsPath), encoding="utf8") as f2:
+            textOpenGuessWords = f2.read()
+
 
         self.listWordsToCheck = self.unpack_words_collection(textOpenForChecking,"\n")
         self.listWordsToGuess = self.unpack_words_to_guess(textOpenGuessWords)
         self.trees = []
-
-        for i in range(maxWordLength-minWordLength+1):
-            self.trees.append(Node.Node(treeDeep))
+        for i in range(self.maxWordLength-self.minWordLength+1):
+            self.trees.append(Node.Node(self.treeDeep))
             for j in self.listWordsToCheck[i]:
-                self.trees[i].add_word(j)    
+                self.trees[i].add_word(j)  
+
 
 
     def unpack_words_collection(self, words, separator):
@@ -68,3 +75,52 @@ class ReadingFile():
     def getGuessWords(self):
         return self.listWordsToGuess
 
+    def add_scores(self, isWin, numberOfLettersInWord):
+        fileName = self.scoresPath
+        if not os.path.exists(os.path.join(self.scriptDir, fileName)): 
+            with open(os.path.join(self.scriptDir, fileName), 'x') as f:
+                f.write('[Scores]\n')
+                f.write('TotalWinScore = 0\n')
+                f.write('TotalLoseScore = 0\n')
+                for i in range(self.minWordLength,self.maxWordLength+1):
+                    f.write(str(i)+'LetterWordsWin = 0\n')
+                    f.write(str(i)+'LetterWordsLose = 0\n')
+
+        scores = ConfigParser()
+        scores.read(os.path.join(self.scriptDir, fileName))
+
+        if isWin:
+            scores['Scores']['TotalWinScore']  = str(int(scores['Scores']['TotalWinScore'])  + 1)
+            scores['Scores'][str(numberOfLettersInWord)+'LetterWordsWin']  = str( int(scores['Scores'][str(numberOfLettersInWord)+'LetterWordsWin'])  + 1)
+        else:
+            scores['Scores']['TotalLoseScore']  = str(int(scores['Scores']['TotalLoseScore'])  + 1)
+            scores['Scores'][str(numberOfLettersInWord)+'LetterWordsLose'] = str( int(scores['Scores'][str(numberOfLettersInWord)+'LetterWordsLose']) + 1)   
+        
+        with open(os.path.join(self.scriptDir, fileName),'w+') as scoreFile:
+            scores.write(scoreFile)
+
+
+    def save_game(self,listOfGuesses):
+        fileName = self.savedGamesPath
+        if not os.path.exists(os.path.join(self.scriptDir, fileName)): 
+            open(os.path.join(self.scriptDir, fileName), 'x')
+        savedGames = ConfigParser()
+        savedGames.read(os.path.join(self.scriptDir, fileName))
+        savedGames.add_section("Game - "+ str(len(savedGames.sections())+1))
+        for i in range(len(listOfGuesses)):
+            savedGames.set("Game - "+ str(len(savedGames.sections())),"Round "+str(i), str(listOfGuesses[i]))  
+        savedGames.set("Game - "+ str(len(savedGames.sections())),"Number of needed words", str(len(listOfGuesses)))     
+        savedGames.set("Game - "+ str(len(savedGames.sections())),"Date ", datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S") )
+        with open(os.path.join(self.scriptDir, fileName),'w+') as gamesFile:
+            savedGames.write(gamesFile)
+
+
+    def read_config(self):
+        fileName = "Config.ini"
+        config = ConfigParser()
+        config.read(os.path.join(self.scriptDir, fileName))
+        return config
+            
+
+    def getConfigFile(self):
+        return self.configFile
--- main.py
+++ main.py
@@ -1,34 +1,145 @@
 from typing import Optional
-from fastapi import FastAPI
 import ReadingFile
 import Node
 import datetime
+import random
+import os
 from timeit import default_timer as timer
 
 
-minWordLength = 4
-maxWordLength = 7
-treeDeep = 3
+read = ReadingFile.ReadingFile()
+configFile = read.getConfigFile()
 
-read = ReadingFile.ReadingFile(minWordLength, maxWordLength, treeDeep)
+minWordLength = int(configFile['base']['minWordLength'])
+maxWordLength = int(configFile['base']['maxWordLength'])
+treeDeep = int(configFile['base']['treeDeep'])
+numberOfRounds = int(configFile['base']['numberOfRounds'])
+
+
 readCheck = read.getCheckingTrees()
-#print(readCheck[0])
 readGuess = read.getGuessWords()
 
+#print(5 + minWordLength - maxWordLength - 1, "\t")
+#print(readCheck[5 + minWordLength - maxWordLength- 1].search_for_word("łódka"))
+'''
 start1 = timer()
 readCheck[1].search_for_word("chlew")
 end1 = timer()
 
 start2 = timer()
-read.listWordsToGuess[1].__contains__("chlew")
+"chlew" in read.listWordsToGuess[1]
 end2 = timer()
-
-
+print("TreeDeep: ", treeDeep)
 print("Struktura drzewiasta:  ", (end1 - start1)*1000," sekund")
-
 print("Lista               :  ", (end2 - start2)*1000," sekund")
+'''
+
+
+def start(readCheck, readGuess):
+    end = False
+    while(not end):
+        dic = {
+                    "ą":  "unknown",
+                    "b":  "unknown",
+                    "a":  "unknown",
+                    "c":  "unknown",
+                    "ć":  "unknown",
+                    "d":  "unknown",
+                    "e":  "unknown",
+                    "ę":  "unknown",
+                    "f":  "unknown",
+                    "g":  "unknown",
+                    "h":  "unknown",
+                    "i":  "unknown",
+                    "j":  "unknown",
+                    "k":  "unknown",
+                    "l":  "unknown",
+                    "ł":  "unknown",
+                    "m":  "unknown",
+                    "n":  "unknown",
+                    "ń":  "unknown",
+                    "o":  "unknown",
+                    "ó":  "unknown",
+                    "p":  "unknown",
+                    "r":  "unknown",
+                    "s":  "unknown",
+                    "ś":  "unknown",
+                    "t":  "unknown",
+                    "u":  "unknown",
+                    "w":  "unknown",
+                    "v":  "unknown",
+                    "x":  "unknown",
+                    "y":  "unknown",
+                    "z":  "unknown",
+                    "ź":  "unknown",
+                    "ż":  "unknown"
+                } 
+
+        print("\n\n","---------------","Wordle","---------------","\n")
+        print("MENU:","\n","1. Graj","\n","2. Wyjdź")
+
+        decision = input()
+        while(not decision.isdecimal() and (decision!=1 and decision!=2)):
+            decision = input("Niepoprawne wprowadzenie, spróbuj ponownie:  ")
+
+        if decision == "1":
+            play_game(readCheck, readGuess, dic)
+        else:
+            end = True    
 
 
 
+def play_game(readCheck, readGuess, dic):
+    wordLength = 4
+    word  = readGuess[wordLength + minWordLength - maxWordLength-1][random.randrange(0,len(readGuess[wordLength + minWordLength - maxWordLength-1]))]
+    print("słow do zgadnięcia: ",word)
 
-#print(readGuess)
+    i = 0
+    end = False
+    roundHistory = []
+    while( i<numberOfRounds and not end ):
+        print("\n")
+        guess = input("Podaj słowo:    ")
+        while(not readCheck[wordLength + minWordLength - maxWordLength-1].search_for_word(guess) or len(guess) != wordLength):
+            guess = input("Błędne słowo, podaj słowo ponownie:    ")
+        resultOfComparison = check_words(word, guess)    
+        roundHistory.append(resultOfComparison) 
+        if word == guess:
+            print("WYGRAŁEŚ!!!")
+            read.add_scores(True,len(word))
+            read.save_game(roundHistory)
+            end = True
+        else:
+            update_dictionary(dic,resultOfComparison)
+        i += 1    
+        print(dic)
+    if end == False:
+        read.add_scores(False,len(word))
+        print("Przegrałeś!!!")
+
+
+def update_dictionary( dic, resultOfComparison):
+    for i in dic.keys():
+        for j in resultOfComparison:
+            if i == j[0]:
+                dic[i] = j[1]     
+
+
+def check_words(word, guess):
+    resultOfComparison = []
+    for i in range(len(word)):
+        resultOfComparison.append([guess[i],"unknown"])
+        for j in range(len(word)):
+            if i == j and guess[i] == word[j]:
+                resultOfComparison[i][1] = "inplace"
+            elif i != j and guess[i] == word[j] and resultOfComparison[i][1] != "inplace":
+                resultOfComparison[i][1] = "known"
+            elif resultOfComparison[i][1] != "inplace" and resultOfComparison[i][1] != "known":
+                resultOfComparison[i][1] = "out"   
+            else:
+                pass    
+    return resultOfComparison
+
+
+start(readCheck,readGuess)
+
